2afcf280566826ff315e80a0646ef4c189169cc6
estcar08 <Carlos.Estrella@theksquaregroup.com>
2021-12-01 18:49:45 -0600

    last update

--- LinkedList.js
+++ LinkedList.js
@@ -89,14 +89,149 @@ class LinkedList {
     return this.head;
   }
 
-  removeMidNode(){
+  removeMidNode() {
     if (this.head == null) return null;
 
     if (this.head.next == null) {
       return null;
     }
-    var copyHead = this.head;
-    const count = this.getSize();
+
+    var slow_ptr = this.head;
+    var fast_ptr = this.head;
+
+    var prev = null;
+
+    while (fast_ptr != null && fast_ptr.next != null) {
+      fast_ptr = fast_ptr.next.next;
+      prev = slow_ptr;
+      slow_ptr = slow_ptr.next;
+    }
+
+    prev.next = slow_ptr.next;
+
+    return this.head;
+  }
+
+  removeNodeByPosition(position) {
+    if (this.head == null || position < 0) return;
+
+    var temp = this.head;
+
+    if (position == 0) {
+      this.head = temp.next;
+      return;
+    }
+
+    for (let i = 0; temp != null && i < position - 1; i++) temp = temp.next;
+
+    if (temp == null || temp.next == null) return;
+
+    var next = temp.next.next;
+
+    temp.next = next;
+  }
+
+  searchInNode(x) {
+    var current = this.head;
+    while (current != null) {
+      if (current.data == x) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  startSort(reverse) {
+    this.mergeSort(this.head, reverse);
+  }
+  mergeSort(head, reverse) {
+    if (head.next == null) return head;
+
+    var mid = this.findMid(head);
+    var head2 = mid.next;
+    mid.next = null;
+    var newHead1 = this.mergeSort(head, reverse);
+    var newHead2 = this.mergeSort(head2, reverse);
+    var finalHead = this.merge(newHead1, newHead2, reverse);
+    return (this.head = finalHead);
+  }
+
+  merge(head1, head2, reverse) {
+    var merged = new ListNode(-1);
+    var temp = merged;
+
+    while (head1 != null && head2 != null) {
+      if (reverse) {
+        if (head1.data > head2.data) {
+          temp.next = head1;
+          head1 = head1.next;
+        } else {
+          temp.next = head2;
+          head2 = head2.next;
+        }
+      } else {
+        if (head1.data < head2.data) {
+          temp.next = head1;
+          head1 = head1.next;
+        } else {
+          temp.next = head2;
+          head2 = head2.next;
+        }
+      }
+
+      temp = temp.next;
+    }
+
+    while (head1 != null) {
+      temp.next = head1;
+      head1 = head1.next;
+      temp = temp.next;
+    }
+
+    while (head2 != null) {
+      temp.next = head2;
+      head2 = head2.next;
+      temp = temp.next;
+    }
+    return merged.next;
+  }
+
+  mergeRev(head1, head2) {
+    var merged = new ListNode(-1);
+    var temp = merged;
+
+    while (head1 != null && head2 != null) {
+      if (head1.data > head2.data) {
+        temp.next = head1;
+        head1 = head1.next;
+      } else {
+        temp.next = head2;
+        head2 = head2.next;
+      }
+      temp = temp.next;
+    }
+
+    while (head1 != null) {
+      temp.next = head1;
+      head1 = head1.next;
+      temp = temp.next;
+    }
+
+    while (head2 != null) {
+      temp.next = head2;
+      head2 = head2.next;
+      temp = temp.next;
+    }
+    return merged.next;
+  }
+
+  findMid(head) {
+    var slow = head,
+      fast = head.next;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
   }
 }
 
@@ -112,5 +247,7 @@ let list = new LinkedList(head);
 list.push(8);
 
 list.appendMid(7);
+
+//console.log(list.searchInNode(8));
 
 list.printLL();
